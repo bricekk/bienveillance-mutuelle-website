aadce997dc2a1f9a0cf02e01b873705116420294
Brice <briceklog@gmail.com>
2022-05-11 16:48:24 +0100

    contact form

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import './contact.scss'
+
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/e50db840-d13b-11ec-a821-6590c8b23a22";
+
+
+const Contact = () => {
+
+
+
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = () => {
+
+        setTimeout(() => {
+
+            setSubmitted(true);
+
+        }, 100);
+
+    };
+
+
+    return (
+        <div className='section__padding'>
+            <h2>Contactez nous</h2><br />
+            <p>Nous vous laissons la possibilit&eacute; de nous laisser un message au cas ou vous auriez a nous dire.</p><br />
+
+            <div className="container">
+                <form action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST">
+                    <div className="row">
+                        <div className="col-25">
+                            <label for="name">Your Name</label>
+                        </div>
+                        <div className="col-75">
+                            <input type="text" id="name" name="name" placeholder="Your name.." required/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-25">
+                            <label for="email">Email</label>
+                        </div>
+                        <div className="col-75">
+                            <input type="email" id="email" name="email" placeholder='example@gmail.com' required/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-25">
+                            <label for="subject">Subject</label>
+                        </div>
+                        <div className="col-75">
+                            <textarea id="subject" name="subject" placeholder="Write something.." style={{ height: "200px" }} required></textarea>
+                        </div>
+                    </div>
+                    <div className="row" style={{ marginTop: "1rem" }}>
+                        <input type="submit" value="Submit" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
